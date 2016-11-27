17bccea288fd4c8af0766b965f0bb62f31e088ed
Phillip Cloud <cpcloud@gmail.com>
2016-11-27 13:58:12 -0800

    Versioneer

--- slumba/__init__.py
+++ slumba/__init__.py
@@ -3,3 +3,7 @@ from .slumba import register_function_pointer
 __all__ = [
     'register_function_pointer',
 ]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
