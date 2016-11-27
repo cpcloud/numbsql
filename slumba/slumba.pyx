9b06a15c9fc686a4c5043159d82f3587580791d5
Phillip Cloud <cpcloud@gmail.com>
2016-11-27 09:25:56 -0800

    Report error messages better

--- slumba/slumba.pyx
+++ slumba/slumba.pyx
@@ -1,5 +1,8 @@
 cdef extern from "sqlite3.h":
     int SQLITE_UTF8
+    int SQLITE_DETERMINISTIC
+    int SQLITE_LIMIT_FUNCTION_ARG
+    int SQLITE_OK
 
     ctypedef struct sqlite3:
         pass
@@ -18,12 +21,14 @@ cdef extern from "sqlite3.h":
         sqlite3 *db,
         const char *function_name,
         int number_of_arguments,
-        int test_representation,
+        int text_representation,
         void *application_data,
         scalarfunc scalar_function,
         stepfunc step_function,
         finalfunc final_function
-    ) nogil
+    )
+
+    const char *sqlite3_errmsg(sqlite3 *db)
 
 
 cdef extern from "pysqlite/connection.h":
@@ -31,21 +36,29 @@ cdef extern from "pysqlite/connection.h":
         cdef sqlite3 *db
 
 
-cpdef int register_function_pointer(
+cpdef object register_function_pointer(
     Connection con,
     const char *name,
     int narg,
-    Py_ssize_t address
-) nogil except -1:
-    with nogil:
-        return sqlite3_create_function(
-            con.db,
-            name,
-            narg,
-            SQLITE_UTF8,
-            NULL,
-            # for some reason Cython fails when using the `scalarfunc` type
-            <void (*)(sqlite3_context*, int, sqlite3_value**)> address,
-            NULL,
-            NULL,
+    Py_ssize_t address,
+    bint is_deterministic=True
+):
+    assert -1 <= narg <= SQLITE_LIMIT_FUNCTION_ARG, \
+        'Number of arguments must be between -1 and {:d}'.format(
+            SQLITE_LIMIT_FUNCTION_ARG
         )
+
+    cdef int result = sqlite3_create_function(
+        con.db,
+        name,
+        narg,
+        SQLITE_UTF8 | (is_deterministic & SQLITE_DETERMINISTIC),
+        NULL,
+        # for some reason Cython fails when using the `scalarfunc` type
+        <void (*)(sqlite3_context*, int, sqlite3_value**)> address,
+        NULL,
+        NULL,
+    )
+
+    if result != SQLITE_OK:
+        raise RuntimeError(sqlite3_errmsg(con.db))
