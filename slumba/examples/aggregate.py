27782d9bbe06f424b371fb3152e04dcc326c4053
Phillip Cloud <cpcloud@gmail.com>
2016-12-03 14:52:36 -0800

    Playing around with aggregates

--- slumba/examples/aggregate.py
+++ slumba/examples/aggregate.py
@@ -1,5 +1,9 @@
 import sqlite3
 import time
+import string
+
+from operator import attrgetter
+from collections import namedtuple
 
 from numba import float64, int64, jitclass
 
@@ -56,7 +60,6 @@ class Cov(object):
         return n / (n - 1) * self.mean12
 
 
-
 @sqlite_udaf(float64(float64))
 @jitclass([
     ('total', float64),
@@ -72,41 +75,114 @@ class Avg(object):
         self.count += 1
 
     def finalize(self):
+        if not self.count:
+            return None
         return self.total / self.count
+
+
+@sqlite_udaf(float64(float64))
+@jitclass([('total', float64), ('count', int64)])
+class Sum(object):
+    def __init__(self):
+        self.total = 0.0
+        self.count = 0
+
+    def step(self, value):
+        self.total += value
+        self.count += 1
+
+    def finalize(self):
+        return None if not self.count else self.total
 
 
 def main():
     import random
 
     con = sqlite3.connect(':memory:')
-    con.execute('CREATE TABLE t (random_numbers DOUBLE PRECISION)')
-    random_numbers = [(random.random(),) for _ in range(5000000)]
+    con.execute("""
+        CREATE TABLE t (
+          id INTEGER PRIMARY KEY,
+          key VARCHAR(1),
+          value DOUBLE PRECISION
+        )
+    """)
+    # con.execute('CREATE INDEX key_index ON t (key)')
 
-    con.executemany('INSERT INTO t VALUES (?)', random_numbers)
+    random_numbers = [
+        (
+            random.choice(string.ascii_lowercase[:2]),
+            random.random()
+        ) for _ in range(5000000)
+    ]
 
-    # new way of registering C functions
-    register_aggregate_function(con, b'myavg', 1,
-            Avg.step.address, Avg.finalize.address)
+    con.executemany(
+        'INSERT INTO t (key, value) VALUES ({})'.format(
+            ', '.join('?' * len(random_numbers[0]))
+        ),
+        random_numbers
+    )
 
-    con.create_aggregate('myavg2', 1, Avg.numba_class.class_type.class_def)
+    cls = Avg
 
-    query1 = 'select myavg(random_numbers) as myavg from t'
-    query2 = 'select myavg2(random_numbers) as oldavg from t'
+    builtin = cls.__name__.lower()
+    cfunc_defined = 'my{}'.format(builtin)
+    python_defined = 'my{}2'.format(builtin)
 
+    # new way of registering UDAFs using cfuncs
+    register_aggregate_function(
+        con,
+        cfunc_defined.encode('utf8'),
+        1,
+        cls.step.address,
+        cls.finalize.address
+    )
 
-    start1 = time.time()
-    exe1 = con.execute(query1)
-    t1 = time.time() - start1
-    result1 = list(exe1)
+    con.create_aggregate(python_defined, 1, cls.class_type.class_def)
 
-    start2 = time.time()
-    exe2 = con.execute(query2)
-    t2 = time.time() - start2
-    result2 = list(exe2)
+    query1 = 'select key, {0}(value) as builtin_{0} from t group by 1'.format(
+        builtin
+    )
+    query2 = 'select key, {0}(value) as cfunc_{0} from t group by 1'.format(
+        cfunc_defined
+    )
+    query3 = 'select key, {0}(value) as python_{0} from t group by 1'.format(
+        python_defined
+    )
 
-    print(result1 == result2)
-    print('t1 == {:.2f}'.format(t1))
-    print('t2 == {:.2f}'.format(t2))
+    # query1 = 'select {0}(value) as builtin_{0} from t'.format(builtin)
+    # query2 = 'select {0}(value) as cfunc_{0} from t'.format(cfunc_defined)
+    # query3 = 'select {0}(value) as python_{0} from t'.format(python_defined)
+
+    queries = {
+        builtin + '_builtin': query1,
+        cfunc_defined + '_cfunc': query2,
+        python_defined + '_python': query3,
+    }
+
+    results = []
+
+    Result = namedtuple('Result', 'name result duration')
+
+    for name, query in queries.items():
+        start = time.time()
+        exe = con.execute(query)
+        duration = time.time() - start
+        result = list(exe)
+        results.append(Result(name=name, result=result, duration=duration))
+
+    results.sort(key=attrgetter('duration'))
+
+    strings = [
+        '{} duration == {:.2f}s | {:d}x faster'.format(
+            name, duration, round(results[-1].duration / duration)
+        )
+        for name, result, duration in results
+    ]
+
+    width = max(map(len, strings))
+
+    for s in strings:
+        print(s.rjust(width, ' '))
 
 
 if __name__ == '__main__':
