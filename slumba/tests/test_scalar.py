e5afb5c3bee36b3469073ec7b69ac122739744ba
Phillip Cloud <cpcloud@gmail.com>
2016-12-03 20:40:41 -0800

    More scalar tests

--- slumba/tests/test_scalar.py
+++ slumba/tests/test_scalar.py
@@ -8,11 +8,23 @@ from slumba import create_function, sqlite_udf
 from numba import float64
 
 
+@sqlite_udf(float64(float64))
+def add_one(x):
+    return x + 1.0 if x is not None else None
+
+
 @pytest.fixture
 def con():
     con = sqlite3.connect(':memory:')
     con.execute("""
         CREATE TABLE t (
+            id INTEGER PRIMARY KEY,
+            key VARCHAR(1),
+            value DOUBLE PRECISION
+        )
+    """)
+    con.execute("""
+        CREATE TABLE s (
             id INTEGER PRIMARY KEY,
             key VARCHAR(1),
             value DOUBLE PRECISION
@@ -27,16 +39,11 @@ def con():
         ('c', 5.0),
     ]
     con.executemany('INSERT INTO t (key, value) VALUES (?, ?)', rows)
+    create_function(con, 'add_one', 1, add_one)
     return con
 
 
-@sqlite_udf(float64(float64))
-def add_one(x):
-    return x + 1.0 if x is not None else None
-
-
 def test_scalar(con):
-    create_function(con, 'add_one', 1, add_one)
     assert list(con.execute('SELECT add_one(value) AS c FROM t')) == [
         (2.0,),
         (3.0,),
@@ -47,5 +54,9 @@ def test_scalar(con):
 
 
 def test_scalar_with_aggregate(con):
-    create_function(con, 'add_one', 1, add_one)
     assert list(con.execute('SELECT sum(add_one(value)) as c FROM t')) == [(20.0,)]
+
+
+def test_scalar_with_empty(con):
+    result = list(con.execute('SELECT add_one(value) as c FROM s'))
+    assert result == []
