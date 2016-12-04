c414443e084af34576daf5d5b122f9e60c7bcb04
Phillip Cloud <cpcloud@gmail.com>
2016-12-03 18:13:05 -0800

    Main entry points for registration

--- slumba/__init__.py
+++ slumba/__init__.py
@@ -14,3 +14,38 @@ __all__ = [
 
 __version__ = get_versions()['version']
 del get_versions
+
+
+def create_function(con, name, num_params, func):
+    """Register a UDF with name `name` with the SQLite connection `con`.
+
+    Parameters
+    ----------
+    con : sqlite3.Connection
+    name : str
+    num_params : int
+    func : cfunc
+    """
+    register_scalar_function(
+        con, name.encode('utf8'), num_params, func.address
+    )
+
+
+def create_aggregate(con, name, num_params, aggregate_class):
+    """Register an aggregate named `name` with the SQLite connection `con`.
+
+    Parameters
+    ----------
+    con : sqlite3.Connection
+    name : str
+    num_params : int
+    aggregate_class : JitClass
+       This class must be decorated with @sqlite_udaf for this function to work
+    """
+    register_aggregate_function(
+        con,
+        name.encode('utf8'),
+        num_params,
+        aggregate_class.step.address,
+        aggregate_class.finalize.address,
+    )
