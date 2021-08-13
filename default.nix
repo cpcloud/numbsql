8aa16bacf6b5134185bcbde55d47626a041fba1e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-13 08:03:21 -0400

    fix: add sqlite to build dependencies

--- default.nix
+++ default.nix
@@ -4,12 +4,19 @@ let
   drv =
     { poetry2nix
     , python
+    , lib
+    , sqlite
     }:
 
     poetry2nix.mkPoetryApplication {
       inherit python;
 
-      projectDir = ./.;
+      pyproject = ./pyproject.toml;
+      poetrylock = ./poetry.lock;
+      src = lib.cleanSource ./.;
+
+      buildInputs = [ sqlite ];
+
       overrides = pkgs.poetry2nix.overrides.withDefaults (_: super: {
         llvmlite = super.llvmlite.overridePythonAttrs (_: {
           preConfigure = ''
