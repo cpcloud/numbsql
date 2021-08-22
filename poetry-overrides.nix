6775c1f5853924722c07374b6ce177ccd0ab6e3d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-22 08:53:20 -0400

    test: run tests in a random order

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -4,9 +4,16 @@
       export LLVM_CONFIG=${llvm.dev}/bin/llvm-config
     '';
   });
+
   tomli = super.tomli.overridePythonAttrs (old: {
     propagatedNativeBuildInputs = (old.propagatedNativeBuildInputs or [ ]) ++ [
       self.flit
+    ];
+  });
+
+  pytest-randomly = super.pytest-randomly.overridePythonAttrs (old: {
+    propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
+      self.importlib-metadata
     ];
   });
 }
