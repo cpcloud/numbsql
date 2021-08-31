09608ffed148963f9ffeccf321c41ca217eb9d7b
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-31 08:42:10 -0400

    build: disable broken watchdog tests

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -22,4 +22,12 @@
       self.jupyter-packaging
     ];
   });
+
+  watchdog = super.watchdog.overrideAttrs (attrs: {
+    disabledTests = (attrs.disabledTests or [ ]) ++ [
+      "test_move_to"
+      "test_move_internal"
+      "test_close_should_terminate_thread"
+    ];
+  });
 }
