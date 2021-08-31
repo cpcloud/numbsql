005256fb12e1b304d2c8836a3cbd755060442e8c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-31 08:03:09 -0400

    build: remove importlib_metadata dependency

--- slumba/__init__.py
+++ slumba/__init__.py
@@ -43,14 +43,7 @@ __all__ = (
     "sqlite_udaf",
 )
 
-try:
-    import importlib.metadata as importlib_metadata
-except ImportError:
-    import importlib_metadata  # type: ignore
-
-__version__ = importlib_metadata.version(__name__)
-
-del importlib_metadata
+__version__ = "3.1.0"
 
 
 def create_function(
