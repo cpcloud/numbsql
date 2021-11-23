74870aaeb1d15a7b32f39abfa1d843ee53219aab
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-11-23 05:29:16 -0500

    build: niv -> flakes

--- shell.nix
+++ shell.nix
@@ -1,42 +1,10 @@
-{ python ? "3.9" }:
-let
-  pkgs = import ./nix;
-  prettier = pkgs.writeShellScriptBin "prettier" ''
-    ${pkgs.nodePackages.prettier}/bin/prettier \
-    --plugin-search-dir "${pkgs.nodePackages.prettier-plugin-toml}/lib" \
-    "$@"
-  '';
-  mkPoetryEnv = python: pkgs.poetry2nix.mkPoetryEnv {
-    inherit python;
-    projectDir = ./.;
-    overrides = pkgs.poetry2nix.overrides.withDefaults (
-      import ./poetry-overrides.nix { inherit pkgs; }
-    );
-    editablePackageSources = {
-      numbsql = ./numbsql;
-    };
-  };
-  shellHook = ''
-    ${(import ./pre-commit.nix).pre-commit-check.shellHook}
-  '';
-  commonBuildInputs = with pkgs; [
-    git
-    niv
-    nix-linter
-    nixpkgs-fmt
-    poetry
-    prettier
-    sqlite
-    cachix
-    commitizen
-  ];
-  name = "python${builtins.replaceStrings [ "." ] [ "" ] python}";
-in
-pkgs.mkShell {
-  name = "numbsql-${name}";
-  inherit shellHook;
-  PYTHONPATH = builtins.toPath ./.;
-  buildInputs = commonBuildInputs ++ [
-    (mkPoetryEnv pkgs.${name})
-  ];
-}
+(import
+  (
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix
