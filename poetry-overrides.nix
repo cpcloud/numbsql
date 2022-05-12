a581f695a80b678f988aca023a1d2416a6af3382
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-12 07:10:56 -0400

    chore(deps): lockfile-maintenance

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,31 +1,11 @@
 { lib, pkgs, stdenv, ... }: self: super: {
-  watchdog = super.watchdog.overrideAttrs (attrs: {
-    disabledTests = (attrs.disabledTests or [ ]) ++ [
-      "test_move_to"
-      "test_move_internal"
-      "test_close_should_terminate_thread"
-    ];
-  });
-
   numba = super.numba.overridePythonAttrs (_: {
     NIX_CFLAGS_COMPILE = lib.optionalString
       stdenv.isDarwin
       "-I${lib.getDev pkgs.libcxx}/include/c++/v1";
   });
 
-  jupyter-core = super.jupyter-core.overridePythonAttrs (attrs: {
-    buildInputs = (attrs.buildInputs or [ ]) ++ [ self.flit-core ];
-  });
-
-  typing-extensions = super.typing-extensions.overridePythonAttrs (attrs: {
-    buildInputs = (attrs.buildInputs or [ ]) ++ [ self.flit-core ];
-  });
-
-  entrypoints = super.entrypoints.overridePythonAttrs (attrs: {
-    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ self.flit ];
-  });
-
-  isort = super.isort.overridePythonAttrs (attrs: {
-    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ self.poetry ];
+  traitlets = super.traitlets.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.flit-core ];
   });
 }
